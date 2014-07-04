30b14c748ccbf26666ac8259788cf792f2644293
asenisa12 <asenisa@abv.bg>
2014-07-04 11:42:18 +0300

    echo serever

---
+++ echo_serv/echoserver.c
@@ -0,0 +1,68 @@
+#include "../lib/unp.h"
+
+
+void str_echo2(int sockfd){
+	ssize_t n;
+	char buf[MAXLINE];
+
+again:
+	while( (n=read(sockfd, buf, MAXLINE)) > 0)
+		write(sockfd, buf, n);
+
+	if(n < 0 && errno ==EINTR)
+		goto again;
+	else if(n<0)
+		perror("read error");
+}
+
+
+
+int main(){
+
+	int listenfd, connfd;
+	pid_t childpid;
+	socklen_t client;
+
+	struct sockaddr_in clientaddr, servaddr;
+
+	if((listenfd = socket(AF_INET, SOCK_STREAM, 0))<0)
+		perror("socket:");
+
+
+	bzero(&servaddr,sizeof(servaddr));
+
+	printf("%d\n",SERV_PORT);
+	servaddr.sin_family =AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+
+	if( (bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)))<0)
+		perror("bind:");
+
+
+	if((listen(listenfd,100))<0)
+		perror("listen:");
+
+
+	for(;;){
+		client = sizeof(clientaddr);
+		connfd = accept(listenfd,(struct sockaddr*)&clientaddr,&clientaddr);
+
+		if ((childpid = fork()) ==0){
+
+			close(listenfd);
+
+			str_echo2(connfd);
+
+			write(connfd,"asen e pich\n",strlen("asen e pich\n"));
+			
+			exit(0);
+		}
+
+		close(connfd);
+	}
+
+	return 0;
+
+}
